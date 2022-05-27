d0dd4282500fc29ba319234cfd31fe5192ec77ff
JorgeFelixC <jorgefelix97@gmail.com>
2022-05-27 12:58:39 -0600

    back code normal

--- class/iobox.php
+++ class/iobox.php
@@ -1,17 +1,14 @@
 <?php
 /*
      Copyright (C) 2007 - 2008  Nicaw
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -51,12 +48,7 @@ public function addCaptcha(){
 public function addInput($name, $type = 'text', $value = '', $length = 100, $readonly = false){
 	if ($readonly) $readonly = ' readonly="readonly"';
 	else $readonly = '';
-	$this->elements[]= '
-	<div class="wrapper-form">
-		<label for="'.$this->name.'__'.$name.'">'.ucfirst($name).'</label>
-		<input class="textfield" id="'.$this->name.'__'.$name.'" name="'.$this->name.'__'.$name.'" type="'.$type.'" maxlength="'.$length.'" value="'.$value.'"'.$readonly.'/>
-	</div>
-	';
+	$this->elements[]= '<input id="'.$this->name.'__'.$name.'" name="'.$this->name.'__'.$name.'" type="'.$type.'" maxlength="'.$length.'" value="'.$value.'"'.$readonly.'/>&nbsp;<label for="'.$this->name.'__'.$name.'">- '.ucfirst($name).'</label>';
 }
 public function addCheckBox($name, $check = false){
 	if ($check) $check = ' checked="checked"';
@@ -67,11 +59,7 @@ public function addTextbox($name,$value = '',$cols = 40,$rows = 10){
 	$this->elements[]= '<textarea name="'.$this->name.'__'.$name.'" cols="'.$cols.'" rows="'.$rows.'">'.$value.'</textarea>';
 }
 public function addSubmit($text){
-	$this->buttons[]= '
-		<div class="wrapper-actions">
-			<input class="btn-primary" type="submit" name="'.$this->name.'__'.$this->name.'" value="'.$text.'"/>
-		</div>		
-		';
+	$this->buttons[]= '<input style="width: 100px; height: 25px;" type="submit" name="'.$this->name.'__'.$this->name.'" value="'.$text.'"/>';
 }
 public function addReload($text){
 	$this->buttons[]= '<input style="width: 100px; height: 25px;" onclick="ajax(\'form\',\''.htmlspecialchars($_SERVER['PHP_SELF']).'\',\'\',true)" type="button" name="'.$this->name.'__'.$this->name.'" value="'.$text.'"/>';
@@ -86,22 +74,19 @@ public function addCode($code){
 	$this->elements[]= $code;
 }
 public function addLabel($code){
-	$this->elements[] = '<h1>'.$code.'</h1>';
+	$this->label = '<legend>'.$code.'</legend>';
 }
 public function getCode(){
-	$code = '';
 	if (isset($_POST['ajax']))
-		$code = '<form id="'.$this->name.'" action="javascript:ajax(\'wrapper-register\',\''.htmlspecialchars($this->target).'\',getParams(document.getElementById(\''.$this->name.'\')),true)" method="post" onmouseup="Cookies.create(\'iobox_x\',document.getElementById(\''.$this->name.'\').style.left,1);Cookies.create(\'iobox_y\',document.getElementById(\''.$this->name.'\').style.top,1);">';
+		$code = '<table cellspacing="10px" onmouseup="Cookies.create(\'iobox_x\',document.getElementById(\'iobox\').style.left,1);Cookies.create(\'iobox_y\',document.getElementById(\'iobox\').style.top,1);" style="visibility:hidden" id="iobox" class="draggable"><tr><td><fieldset>'.$this->label.'<form id="'.$this->name.'" action="javascript:ajax(\'form\',\''.htmlspecialchars($this->target).'\',getParams(document.getElementById(\''.$this->name.'\')),true)" method="post">';
 	else
 		$code = '<div id="iobox" class="iobox"><fieldset>'.$this->label.'<form id="'.$this->name.'" action="'.htmlspecialchars($this->target).'" method="post">';
-	
 	foreach ($this->elements as $element)
-		$code.= $element;
-	// $code.= '<hr style="margin: 10px 2px 2px 2px; padding: 0;"/> | ';
+		$code.= $element."<br/><div style=\"margin-top: 5px;\"></div>\r\n";
+	$code.= '<hr style="margin: 10px 2px 2px 2px; padding: 0;"/> | ';
 	foreach ($this->buttons as $button)
-		$code.= $button;
-
-	$code.= '</form>';
+		$code.= $button." | \r\n";
+	$code.= '</form></fieldset></td></tr></table>';
 	return $code;
 }
 public function show(){
